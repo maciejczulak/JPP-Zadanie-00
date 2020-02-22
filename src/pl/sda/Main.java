7041ecd871dd70d15c01b56878f45aa1b0cee3bb
Maciej Czulak <maciejczulak@o2.pl>
2020-02-22 13:31:59 +0100

    Added BMI calculator

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,17 @@
 package pl.sda;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Height in m:");
+        Double height = scanner.nextDouble();
+        System.out.println("Wieght in kg:");
+        Double wieght = scanner.nextDouble();
+        Double BMI = wieght / (Math.pow(height,2));
+        System.out.println("Your BMI:" + BMI);
+
+
+
+
     }
 }
